389b6bf8abcb6a677b731edc3cd2f936c852ff8c
Ben Long <wblong9115@gmail.com>
2016-01-28 12:14:07 +0000

    fix ci script2

--- run_xctest.sh
+++ run_xctest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+xcodebuild -project TidyJSON.xcodeproj -list
 xcodebuild -project TidyJSON.xcodeproj -scheme "TidyJSON-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
 
 xcodebuild -project TidyJSON.xcodeproj -scheme "TidyJSON-OSX" test
