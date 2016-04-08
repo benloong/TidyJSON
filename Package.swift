9bf7cffd35ae2091a6b4dd954c9d3f8d8254761c
Daniel Leping <dileping@gmail.com>
2016-04-08 20:02:04 +0300

    Proper versions of dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "TidyJSON",
     dependencies: [
-        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", majorVersion: 0),
-        .Package(url: "https://github.com/crossroadlabs/Foundation3.git", majorVersion: 0),
-        .Package(url: "https://github.com/crossroadlabs/XCTest3.git", majorVersion: 0)
+        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/crossroadlabs/Foundation3.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/crossroadlabs/XCTest3.git", majorVersion: 0, minor: 1)
     ]
 )
